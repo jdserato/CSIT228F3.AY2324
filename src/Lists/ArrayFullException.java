a1112f3a4a64a3368907206860114f9237be2088
Jay Vince Serato <jayvince.serato@gmail.com>
2024-02-16 07:43:53 +0800

    Initial commit

--- src/Lists/ArrayFullException.java
+++
@@ -1,7 +0,0 @@
-package Lists;
-
-public class ArrayFullException extends Exception {
-    public ArrayFullException(Object num) {
-        super("The array is full and " + num + " cannot be inserted.");
-    }
-}
--- src/Lists/InvalidPositionException.java
+++
@@ -1,7 +0,0 @@
-package Lists;
-
-public class InvalidPositionException extends IllegalArgumentException {
-    public InvalidPositionException(int upper) {
-        super("Position must be between 1 and " + upper);
-    }
-}
